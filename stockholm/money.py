261cec0da1487fef200bfa0280e7de8aad741856
Carl Oscar Aaro <hello@carloscar.com>
2019-11-28 00:10:16 +0100

    Moved function

--- stockholm/money.py
+++ stockholm/money.py
@@ -315,9 +315,6 @@ class Money:
     def as_int(self) -> int:
         return int(self)
 
-    def to_integral(self) -> "Money":
-        return self.__round__(0)
-
     def as_float(self) -> float:
         return float(self)
 
@@ -335,6 +332,9 @@ class Money:
 
     def sub(self, other: Any, from_sub_units: Optional[bool] = None) -> "Money":
         return self.subtract(other, from_sub_units=from_sub_units)
+
+    def to_integral(self) -> "Money":
+        return self.__round__(0)
 
     def to_currency(self, currency: Optional[Union[BaseCurrency, str]]) -> "Money":
         return Money(self, currency=currency)
